1e728afafcf19599e6b627a1e6a15a37331c145f
srujan <srujan18@hotmail.com>
2020-08-24 15:53:37 -0400

    staging area sorted

--- src/main/scala/ExtTables.scala
+++ src/main/scala/ExtTables.scala
@@ -5,26 +5,24 @@ class ExtTables extends Staging {
     stmt execute """DROP TABLE IF EXISTS ext_frequencies"""
     stmt execute """DROP TABLE IF EXISTS ext_calendar_dates"""
     stmt execute
-      """CREATE EXTERNAL TABLE fall2019_snehith.ext_trips (
-        |route_id               INT,
-        |service_id             STRING,
-        |trip_id                STRING,
-        |trip_headsign          STRING,
-        |direction_id           STRING,
-        |shape_id               STRING,
-        |wheelchair_accessible  STRING,
-        |note_fr                STRING,
-        |note_en                STRING
-        |)
+      """CREATE EXTERNAL TABLE fall2019_snehith.ext_routes (
+        |`route_id` int,
+        |`agency_id` string,
+        |`route_short_name` int,
+        |`route_long_name` string,
+        |`route_type` int
+        |`route_url` string,
+        |`route_color` string,
+        |`route_text_color` string
         |ROW FORMAT DELIMITED
         |FIELDS TERMINATED BY ','
         |STORED AS TEXTFILE
-        |LOCATION 's3://course8-aws/assignment1/trips/'
+        |LOCATION 's3://course8-aws/assignment1/routes/'
         |TBLPROPERTIES (
         | "skip.header.line.count" = "1",
         |"serialization.null.format" = "")""".stripMargin
 
-    println("ext_trips TABLE was CREATED")
+    println("ext_routess TABLE was CREATED")
     stmt execute
       """CREATE EXTERNAL TABLE fall2019_snehith.ext_calendar_dates (
         |service_id       STRING,
