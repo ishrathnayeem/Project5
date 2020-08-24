d56f9b1fffdf66a5d9fe5f6ae88ac0afe35b2578
snehith <ksnehith@live.com>
2020-08-24 04:56:50 -0400

    adding amazon athena client

--- src/main/scala/Staging.scala
+++ src/main/scala/Staging.scala
@@ -1,29 +1,14 @@
-import java.io.ByteArrayInputStream
 import java.sql.{Connection, DriverManager, Statement}
 
 import com.amazonaws.regions.Regions
-import com.amazonaws.services.s3.model.ObjectMetadata
 import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
 
 trait Staging extends App {
-  val s3Client: AmazonS3 = AmazonS3ClientBuilder
+  val s3client: AmazonS3 = AmazonS3ClientBuilder
     .standard()
     //.withCredentials(new AWSStaticCredentialsProvider(credentials))
     .withRegion(Regions.US_EAST_1)
     .build()
-  val bucketname=""
-  val filepath = "/home/snehith/course8/"
-  val folder1 ="assignment1/"
-  val meta = new ObjectMetadata()
-  meta.setContentLength(0)
-  val empty = new ByteArrayInputStream(new Array[Byte](0))
-  s3Client.putObject(bucketname,folder1,empty,meta)
-  val tripsname = "assignment1/trips/"
-  val frequenciesname= "assignment1/frequencies/"
-  val calendar_datename = "assignment1/calendar_dates/"
-  val trips=s3Client.putObject(bucketname,tripsname,empty,meta)
-  val frequencies = s3Client.putObject(bucketname,frequenciesname,empty,meta)
-  val calendar_dates = s3Client.putObject(bucketname,calendar_datename,empty,meta)
 
   val driverName: String = "com.simba.athena.jdbc.Driver"
   Class.forName(driverName)
@@ -33,9 +18,23 @@ trait Staging extends App {
       "AwsCredentialsProviderArguments=default;")
 
   val stmt: Statement = connection.createStatement()
+  val bucketname = "course8-aws"
+  val folder= "assignment8"
 
-
-
-
+  val tripsname = "assignment1/trips/"
+  val frequenciesname = "assignment1/frequencies/"
+  val calendar_datename = "assignment1/calendar_dates/"
+  val tripfile = "assignment1/trips/trips.txt"
+  val frequencyfile= "assignment1/frequenciesname/frequencies.txt"
+  val calendardatesfile ="assignment1/calenda_dates/calendar_dates.txt"
+  val tloc= "/home/snehith/Documents/stm/trips.txt"
+  val floc ="/home/snehith/Documents/stm/frequencies.txt"
+  val caloc = "//home/snehith/Documents/stm/calendar_dates.txt"
+  if (s3client.doesBucketExistV2(bucketname))
+  {
+    println("already exist")
+    s3client.deleteBucket(bucketname)
+    println("deldeted")
+  }
 
 }
